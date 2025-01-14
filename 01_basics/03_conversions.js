adff7d59a1e1a10cfae2b3e01369b0c0f91179bf
rakshithkumar608 <rakshithkumab01718@gmail.com>
2025-01-14 05:51:24 +0000

    conversions

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,23 @@
+let score = "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let ValueInNumber = Number(score)
+console.log(typeof ValueInNumber);
+console.log(ValueInNumber);
+
+// '33" => 33
+// "33abc" => Nan
+//true => 1; false => 0
+
+let isLoogedIn = "rakshith"
+
+let booleanIsLoggedin = Boolean(isLoogedIn)
+// console.log(booleanIsLoggedIn);
+
+ let someNumber = 33
+
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ 
