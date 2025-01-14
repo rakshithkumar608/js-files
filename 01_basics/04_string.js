8d5a1adda5b311c90d3c1a1850b5eee496e45f74
rakshithkumar608 <rakshithkumab01718@gmail.com>
2025-01-14 13:25:41 +0000

    strings

---
+++ 01_basics/04_string.js
@@ -0,0 +1,32 @@
+//**************operations ************* */
+
+// let value = 3
+// let negValue = -value
+// console.log("negValue");
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+// let str1 = "hello"
+// let str2 = "rakshith"
+
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 200
+gameCounter++;
+console.log(gameCounter);
