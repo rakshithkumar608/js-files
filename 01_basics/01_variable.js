27dea2bda6f1cd176247f1c6bf89375579e5fa7d
rakshithkumar608 <rakshithkumab01718@gmail.com>
2025-01-14 05:31:44 +0000

    datatypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 122343
+let accountEmail = "rakshithkumarb089@gmail.com"
+var acountPassword = "1343"
+accountCity = "banglore"
+
+
+// accountId = 2
+
+accountEmail = "he@hc.com"
+accountpassword = "43235"
+accountCity = "jaipur"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountpassword, accountCity]);
