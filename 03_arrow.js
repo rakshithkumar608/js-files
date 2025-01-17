1bfefed416687dad4038af13b4c131025c2bf47b
rakshithkumar608 <rakshithkumab01718@gmail.com>
2025-01-17 13:02:21 +0000

    03_arrow

---
+++ 03_arrow.js
@@ -0,0 +1,11 @@
+const user = {
+    username: "rakshith",
+    price: 999,
+
+    welcomeMessage: function(){
+        console.log('${this.username}, welcome to website');
+        
+    }
+}
+
+user.welcomeMessage()
